2ad4784dec213ed11826f4da58cfd6ae38797d00
landscape waco <mustafatahir14.mt@gmail.com>
2026-06-30 22:02:47 +0500

    direct push

--- src/components/EightSection.tsx
+++ src/components/EightSection.tsx
@@ -344,7 +344,7 @@ export default function EightSection() {
 
             {/* Connect Link Call to Action */}
             <div className="link-wrapper z-20">
-                <a href="#" style={linkStyle}>
+                <a href="/contact-form" style={linkStyle}>
                     LETS CONNECT
                 </a>
             </div>
